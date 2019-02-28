4fb00bf5822b590a2c2c92bda12aaffb9520a6c5
Dennis Mo <dennismoyunfei@gmail.com>
2019-02-28 13:52:59 -0500

    Initial Commit

---
+++ Smarties/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Smarties
+//
+//  Created by Dennis Mo on 2/28/19.
+//  Copyright © 2019 Dennis Mo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
